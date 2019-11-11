746ac9369d67379327102dd7b4743a5fd4723e67
hcp3751 <hcp3751@163.com>
2019-11-11 23:04:55 +0800

    网易云

---
+++ README.md
@@ -0,0 +1,20 @@
+# NgWyy
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.0.
+
+## 启动接口（[网易云音乐接口](NeteaseCloudMusicApi)）：
+- git clone https://github.com/Binaryify/NeteaseCloudMusicApi.git
+- npm i
+- npm start
+
+
+## 启动项目：
+- git clone https://github.com/lycHub/ng-wyy.git
+- npm i
+- npm start
+
+
+## 视频地址：
+- B站：https://www.bilibili.com/video/av70355308
+- 网易云课堂：https://study.163.com/course/courseMain.htm?share=2&shareId=480000001947524&courseId=1209529841&_trace_c_p_k2_=832892bbbabb45d2889f5308a17244e8
+- 51cto: https://edu.51cto.com/course/19942.html
---
+++ src/app/store/selectors/member.selector.ts
@@ -0,0 +1,9 @@
+import { createSelector } from '@ngrx/store';
+import { MemberState } from '../reducers/member.reducer';
+
+const selectMemberStates = (state: MemberState) => state;
+export const getModalVisible = createSelector(selectMemberStates, (state: MemberState) => state.modalVisible);
+export const getModalType = createSelector(selectMemberStates, (state: MemberState) => state.modalType);
+export const getUserId = createSelector(selectMemberStates, (state: MemberState) => state.userId);
+export const getLikeId = createSelector(selectMemberStates, (state: MemberState) => state.likeId);
+export const getShareInfo = createSelector(selectMemberStates, (state: MemberState) => state.shareInfo);
